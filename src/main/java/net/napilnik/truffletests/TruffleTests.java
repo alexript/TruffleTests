e41c282004df2637ce586b6cd9f8e49e903635a5
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-06-01 11:46:59 +0300

    initial commit

---
+++ src/main/java/net/napilnik/truffletests/TruffleTests.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.napilnik.truffletests;
+
+/**
+ *
+ * @author malyshev
+ */
+public class TruffleTests {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
